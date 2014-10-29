d32575a8117d620a2e1d77fe985a4af1798a4150
Robert Fryman <robert.fryman@gmail.com>
2014-10-28 19:57:35 -0500

    seed character races

---
+++ db/seeds/000_character_race.rb
@@ -0,0 +1,23 @@
+CharacterRace.create([
+  {
+    name: 'Dwarf'
+  },
+  {
+    name: 'Elf'
+  },
+  {
+    name: 'Gnome'
+  },
+  {
+    name: 'Half-Elf'
+  },
+  {
+    name: 'Halfling'
+  },
+  {
+    name: 'Half-Orc'
+  },
+  {
+    name: 'Human'
+  }
+])
