eb375d5b2c1d0bac1b073dcf6a9f97fdc8fdd1fe
Robert Fryman <robert.fryman@gmail.com>
2014-10-29 20:49:00 -0500

    added seed data for character class

---
+++ db/seeds/001_character_class.rb
@@ -0,0 +1,32 @@
+CharacterClass.create([
+  {
+    name: 'Cleric'
+  },
+  {
+    name: 'Druid'
+  },
+  {
+    name: 'Fighter'
+  },
+  {
+    name: 'Paladin'
+  },
+  {
+    name: 'Ranger'
+  },
+  {
+    name: 'Magic-User'
+  },
+  {
+    name: 'Illusionist'
+  },
+  {
+    name: 'Thief'
+  },
+  {
+    name: 'Assassin'
+  },
+  {
+    name: 'Monk'
+  }
+])
